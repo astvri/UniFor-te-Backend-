f556d3db96fae07bc23805cf9942a9c3e06305af
astvri <pehgyt@gmail.com>
2025-06-04 16:49:50 -0300

    supabaseClient

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -2,10 +2,10 @@ import 'dotenv/config'; // Garante que o .env será carregado
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SECRET_KEY; // ou use PUBLIC_KEY para o frontend
+const supabaseKey = process.env.SUPABASE_ANON_KEY; // aqui: SUPABASE_ANON_KEY conforme seu .env
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('SUPABASE_URL ou SUPABASE_SECRET_KEY não definidos no .env');
+  throw new Error('SUPABASE_URL ou SUPABASE_ANON_KEY não definidos no .env');
 }
 
 const supabase = createClient(supabaseUrl, supabaseKey);
